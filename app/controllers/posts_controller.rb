22b50551229ba4df531269df75229297034f913a
Anas Hmamouch <anashmamouch90@gmail.com>
2015-07-14 16:33:40 +0000

    :wink:

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,6 @@
 class PostsController < ApplicationController
   before_action  :find_post, only:  [:show, :edit, :update, :destroy]
+  before_action  :authenticate_user!, except: [:index, :show]
   respond_to :html, :json, :xml
 
   def index
@@ -19,11 +20,11 @@ class PostsController < ApplicationController
   end
   #Responsible for rendering a form to create a new post Http GET Method
   def new
-    @post = Post.new
+    @post = current_user.posts.build
   end
   #Responsible for creating the post inside the db Http POST Method
   def create
-    @post = Post.new(post_params)
+    @post = current_user.posts.build(post_params)
       if @post.save
         redirect_to @post
       else
