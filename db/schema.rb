22ddb9c08bfde8b60b8ecc4628a52a9f8a527170
Anas Hmamouch <anashmamouch90@gmail.com>
2015-07-14 17:18:25 +0000

    Add name to users.:stuck_out_tongue_closed_eyes:

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150714160433) do
+ActiveRecord::Schema.define(version: 20150714163757) do
 
   create_table "posts", force: :cascade do |t|
     t.string   "title"
@@ -35,6 +35,7 @@ ActiveRecord::Schema.define(version: 20150714160433) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
+    t.string   "name"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
